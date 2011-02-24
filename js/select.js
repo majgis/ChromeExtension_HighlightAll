8b2648361ee2b544f0e2b3a0a9ef5d70218859e1
Michael Jackson <majgis@gmail.com>
2011-02-23 22:41:31 -0800

    Version 0.27
    Added comments to select.js
    Ignore right clicks to eliminate odd behavior resulting from CTRL
    Removed multi-browser support for getting text selection
    
    ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- js/select.js
+++ js/select.js
@@ -12,33 +12,49 @@ document.onmouseup = highlightSelection;
 //Highlight all occurances of the current selection
 function highlightSelection(e) 
 {	
+
+	//Skip this section if mouse event is undefined
 	if (e != undefined)
 	{
+	
+		//Ignore right clicks; avoids odd behavior with CTRL key
+		if (e.button == 2)
+		{
+			return;
+		}
+
+		//Exit if CTRL key is held while auto highlight is checked on
 		if(imedBool && e.ctrlKey)
 		{
 			return;
 		}
 		
+		//Exit if CTRL key not held and auto highlight is checked off
 		if(!imedBool && !e.ctrlKey)
 		{
 			return;
 		}
 	}
 	
-	var selectedText = getSelectedText().replace(/^\s+|\s+$/g, "");
+	var selectedText = window.getSelection().toString().replace(/^\s+|\s+$/g, "");
 	var testText = selectedText.toLowerCase();
 	
+	//Exit if selection is whitespace or what was previously selected
 	if (selectedText == '' || lastText == testText)
 	{
 		return;
 	}
 	
+	// Clear all highlights if requested
 	if (clearBetweenSelections)
 	{
 		clearHighlightsOnPage();
 	}
 	
+	//Perform highlighting
 	localSearchHighlight(selectedText, singleSearch);
+	
+	//Store processed selection for next time this method is called
 	lastText = testText;
 }
 
@@ -46,31 +62,7 @@ function highlightSelection(e)
 // Clears all highlights on the page
 function clearHighlightsOnPage()
 {
-	unhighlight(document.getElementsByTagName('body')[0])
-	return;
-	
-}
-
-//Returns text currently selected in document
-//Reference: http://www.codetoad.com/javascript_get_selected_text.asp
-function getSelectedText()
-{
-	var txt;
-	
-	if (window.getSelection)
-	{
-		txt = window.getSelection();
-	}
-	else if (document.getSelection)
-	{
-		txt = document.getSelection();
-	}
-	else if (document.selection)
-	{
-		txt = document.selection.createRange().text;
-	}
-	
-	return '' + txt;
+	unhighlight(document.getElementsByTagName('body')[0]);	
 }
 
 function updateBooleans(clearBool, highlightOnSelect, singleBool)
