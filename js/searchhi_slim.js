fadc7cfa9708309b5d8176425293719f7dc26c32
Michael Jackson <majgis@gmail.com>
2011-07-07 22:32:01 -0700

    Corrected word limit safeguard to prevent browser from crashing

--- js/searchhi_slim.js
+++ js/searchhi_slim.js
@@ -1,9 +1,9 @@
-/* New from Rob Nitti, who credits 
- * http://bytes.com/groups/javascript/145532-replace-french-characters-form-inp
- * The code finds accented vowels and replaces them with their unaccented version. */
+// Main engine for highlighting
+
 function colorGenerator(random)
 {
-    function hexshort() {
+    function hexshort() 
+    {
         return ((~~(Math.random()*0x10))<<4) | (~~(Math.random()*0x10));
     }
     return function()
@@ -19,28 +19,38 @@ function colorGenerator(random)
     }
 }
 
+/* New from Rob Nitti, who credits 
+ * http://bytes.com/groups/javascript/145532-replace-french-characters-form-inp
+ * The code finds accented vowels and replaces them with their unaccented version. 
+ */
 function stripVowelAccent(str)
 {
-	var rExps=[ /[\xC0-\xC2]/g, /[\xE0-\xE2]/g,
+	var rExps =
+	[ 
+		/[\xC0-\xC2]/g, /[\xE0-\xE2]/g,
 		/[\xC8-\xCA]/g, /[\xE8-\xEB]/g,
 		/[\xCC-\xCE]/g, /[\xEC-\xEE]/g,
 		/[\xD2-\xD4]/g, /[\xF2-\xF4]/g,
-		/[\xD9-\xDB]/g, /[\xF9-\xFB]/g ];
+		/[\xD9-\xDB]/g, /[\xF9-\xFB]/g 
+	];
 
-	var repChar=['A','a','E','e','I','i','O','o','U','u'];
+	var repChar = ['A','a','E','e','I','i','O','o','U','u'];
 
 	for(var i=0; i<rExps.length; ++i)
+	{
 		str=str.replace(rExps[i],repChar[i]);
+	}
 
 	return str;
 }
 
-/* Modification of */
-/* http://www.kryogenix.org/code/browser/searchhi/ */
-/* See: */
-/*   http://www.tedpavlic.com/post_highlighting_search_results_with_ted_searchhi_javascript.php */    
-/*   http://www.tedpavlic.com/post_inpage_highlighting_example.php */
-/* for additional modifications of this base code. */
+/* Modification of 
+ * http://www.kryogenix.org/code/browser/searchhi/ 
+ * See: 
+ *   http://www.tedpavlic.com/post_highlighting_search_results_with_ted_searchhi_javascript.php 
+ *   http://www.tedpavlic.com/post_inpage_highlighting_example.php 
+ * for additional modifications of this base code. 
+ */
 function highlightWord(node,word,doc) {
      doc = typeof(doc) != 'undefined' ? doc : document;
     var hinodes = [], coll;
@@ -104,29 +114,29 @@ function unhighlight(node) {
 	}
 }
 
-//majgis:  added singleSearch option
+
 function localSearchHighlight(searchStr, singleWordSearch, doc) 
 {
+	var MAX_WORDS = 100; //limit for words to search, if unlimited, browser may crash
     doc = typeof(doc) != 'undefined' ? doc : document;
 	
-	if (!doc.createElement) return;
+	if (!doc.createElement) 
+	{
+		return;
+	}
 	
 	// Trim leading and trailing spaces after unescaping
 	searchstr = unescape(searchStr).replace(/^\s+|\s+$/g, "");
 	
-	if( searchStr == '' ) return;
+	if( searchStr == '' ) 
+	{
+		return;
+	}
 	
 	//majgis: Single search option
 	if(singleWordSearch)
 	{
 		phrases = searchStr.replace(/\+/g,' ').split(/\"/);
-		
-		//limit length to prevent crashing browser
-		max=25
-		if (phrases.length > max)
-		{
-			phrases.splice(0,phrases.length - 100);
-		}
 	}
 	else
 	{
@@ -136,23 +146,52 @@ function localSearchHighlight(searchStr, singleWordSearch, doc)
 	var hinodes = [];
 	colorGen = colorGenerator(singleWordSearch || !clearBetweenSelections);
 	
-	for(p=0;p<phrases.length;p++) {
-	        phrases[p] = unescape(phrases[p]).replace(/^\s+|\s+$/g, "");
-		if( phrases[p] == '' ) continue;
-		if( p % 2 == 0 ) words = phrases[p].replace(/([+,()]|%(29|28)|\W+(AND|OR)\W+)/g,' ').split(/\s+/);
-		else { words=Array(1); words[0] = phrases[p]; }
-        for (w=0;w<words.length;w++) {
-            if( words[w] == '' ) continue;
+	for(p=0; p < phrases.length; p++) 
+	{
+	    phrases[p] = unescape(phrases[p]).replace(/^\s+|\s+$/g, "");
+	    
+		if( phrases[p] == '' ) 
+		{
+			continue;
+		}
+		
+		if( p % 2 == 0 ) 
+		{
+			words = phrases[p].replace(/([+,()]|%(29|28)|\W+(AND|OR)\W+)/g,' ').split(/\s+/);
+		}
+		else 
+		{ 
+			words=Array(1); 
+			words[0] = phrases[p]; 
+		}
+		
+		//limit length to prevent crashing browser
+		if (words.length > MAX_WORDS)
+		{
+			words.splice(MAX_WORDS - 1, phrases.length - MAX_WORDS);
+		}
+		
+        for (w=0; w < words.length; w++) 
+        {
+            if( words[w] == '' ) 
+            {
+            	continue;
+            }
+            
             col = colorGen();
-            hinodes = highlightWord(doc.getElementsByTagName("body")[0],words[w],doc,col);
-            for (x=0;x<hinodes.length;x++) {
+            hinodes = highlightWord(doc.getElementsByTagName("body")[0], words[w], doc, col);
+            
+            for (x=0; x < hinodes.length; x++) 
+            {
                 hinodes[x].className += col[0];
             }
+            
             cssstr += cssstr.indexOf(col[0]) == -1 ? ".searchword" + col[0] + "{background-color:hsl(" + col[1] + ")}" : "";
         }
 	}
 	
-	if (cssstr.length) {
+	if (cssstr.length) 
+	{
 	    updateStyleNode(cssstr);
 	}
 }
